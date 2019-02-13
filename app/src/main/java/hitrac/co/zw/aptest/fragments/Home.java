8b16daa849631b7e837f5cfda4c4c704e7402052
truzane <truzane@hitrac.co.zw>
2019-02-13 11:15:20 +0200

    initial commit

---
+++ app/src/main/java/hitrac/co/zw/aptest/fragments/Home.java
@@ -0,0 +1,111 @@
+package hitrac.co.zw.aptest.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import hitrac.co.zw.aptest.R;
+
+
+import static hitrac.co.zw.aptest.Dashboard.loginmenuItem;
+
+
+public class Home extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public Button syllabus1;
+    public Button syllabus2;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public Home() {
+
+    }
+
+
+    public static Home newInstance(String param1, String param2) {
+        Home fragment = new Home();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView= inflater.inflate(R.layout.fragment_home, container, false);
+
+        syllabus1=(Button)rootView.findViewById(R.id.syllabus1);
+        syllabus2=(Button)rootView.findViewById(R.id.syllabus2);
+        loginmenuItem.setTitle("Logout");
+
+        syllabus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment= new Results();
+                FragmentManager fragmentManager= getFragmentManager();
+                FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,fragment);
+                fragmentTransaction.commit();
+                fragmentTransaction.addToBackStack(null);
+            }
+        });
+
+
+
+        return rootView;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
